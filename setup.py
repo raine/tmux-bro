e0d8ff5fe93e073336ec64b400c05067c73f9017
Raine Virta <raine.virta@gmail.com>
2025-03-06 21:52:54 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name="tmux-bro",
+    version="0.1.0",
+    author="raine",
+    author_email="raine.virta@gmail.com",  # Replace with actual email
+    description="A smart tmux session manager that sets up project-specific sessions automatically",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raine/tmux-bro",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "tmux-bro=main:main",
+        ],
+    },
+)
